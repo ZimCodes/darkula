1f4498f73b10b8f84d619abd199a018f638bc99a
ZimCodes <23222943+ZimCodes@users.noreply.github.com>
2025-10-12 19:31:05 -0500

    - Removes notification feature - Adds .editorconfig

--- src/main/kotlin/com/darkulatheme/jetbrains/DarkulaMeta.kt
+++
@@ -1,9 +0,0 @@
-package com.darkulatheme.jetbrains
-
-import com.intellij.ide.plugins.PluginManagerCore
-import com.intellij.openapi.extensions.PluginId
-
-object DarkulaMeta {
-    val currentVersion: String
-        get() = PluginManagerCore.getPlugin(PluginId.getId("com.zimcodes.idea"))?.version ?: ""
-}
